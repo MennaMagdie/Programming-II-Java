7c5603ab2d082c392b5307503edb9f3bde64ebfa
Menna Magdy <73966691+MennaMagdie@users.noreply.github.com>
2023-11-11 21:33:25 +0200

    Update FileNames.java

--- src/constants/FileNames.java
+++ src/constants/FileNames.java
@@ -2,7 +2,7 @@ package constants;
 
 public interface FileNames {
 
-    String PRODUCT_FILENAME = "D:\\Uni\\Term-5\\Programming-2\\inventory_elsobh\\Products.txt";
-    String EMPLOYEE_FILENAME = "D:\\Uni\\Term-5\\Programming-2\\inventory_elsobh\\Employee.txt";
-    String CUSTOMER_PRODUCT_FILENAME = "D:\\Uni\\Term-5\\Programming-2\\inventory_elsobh\\CustomerProducts.txt";
+    String PRODUCT_FILENAME = "Products.txt";
+    String EMPLOYEE_FILENAME = "Employee.txt";
+    String CUSTOMER_PRODUCT_FILENAME = "CustomerProducts.txt";
 }
